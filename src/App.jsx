8bde80b1944e304328c6bbfc25cfc4fc6f058618
popescuadi15 <popescuadi05@gmail.com>
2024-04-06 13:43:11 +0300

    Added the function for complete task

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,25 @@ function App() {
     const task = {
       id: toDoList.length === 0 ? 1 : toDoList[toDoList.length - 1].id + 1,
       taskName: newTask,
+      completed: false,
     };
     setToDoList([...toDoList, task]);
   };
 
   const deleteTask = (id) => {
     setToDoList(toDoList.filter((task) => task.id !== id));
+  };
+
+  const completeTask = (id) => {
+    setToDoList(
+      toDoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
   };
 
   return (
@@ -32,7 +45,9 @@ function App() {
             <Task
               taskName={task.taskName}
               id={task.id}
+              completed={task.completed}
               deleteTask={deleteTask}
+              completeTask={completeTask}
             />
           );
         })}
--- src/Task.jsx
+++ src/Task.jsx
@@ -1,8 +1,13 @@
 export function Task(props) {
   return (
-    <div className="task">
+    <div
+      className="task"
+      style={{ backgroundColor: props.completed ? "green" : "white" }}
+    >
       <h1>{props.taskName}</h1>
-      <button onClick={() => props.deleteTask(props.id)}>x</button>
+
+      <button onClick={() => props.completeTask(props.id)}>complete</button>
+      <button onClick={() => props.deleteTask(props.id)}>delete</button>
     </div>
   );
 }
